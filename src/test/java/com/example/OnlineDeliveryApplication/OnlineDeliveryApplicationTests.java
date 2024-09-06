dfffd946f12c66f344147f2bc90b7358a6bceadf
MohanReddy <mohanreddyderedla148@gmail.com>
2024-09-06 12:38:58 +0530

    Initial commit

---
+++ src/test/java/com/example/OnlineDeliveryApplication/OnlineDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnlineDeliveryApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
